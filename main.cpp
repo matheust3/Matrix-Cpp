6a63074b75e89e8e64179431f82f5870c79f5069
matheust3 <matheus.toniolli@hotmail.com>
2018-05-29 16:41:53 -0400

    Array to matrix

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
 
     float a[2][3] = {{1, 2, 3}, {2, 3, 5}};
     matrix m = matrix(a);
+    m.show();
     return 0;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -6,25 +6,51 @@
 
 class matrix
 {
-  private:
-    unsigned int _numRows;
-    unsigned int _numColumns;
-    float **_matrix;
+private:
+  unsigned int _numRows;
+  unsigned int _numColumns;
+  float **_matrix;
 
-  public:
-    template <typename T, std::size_t N, std::size_t M>
-    matrix(T (&a)[N][M]);
-    ~matrix();
+public:
+  void show();
+  template <typename T, std::size_t N, std::size_t M>
+  matrix(T (&a)[N][M]);
+  ~matrix();
 };
 template <typename T, std::size_t N, std::size_t M>
 matrix::matrix(T (&a)[N][M])
 {
-    _numRows = N;
-    _numColumns = M;
+  _numRows = N;
+  _numColumns = M;
+  _matrix = new float *[_numRows];
+  for (unsigned int i = 0; i < _numRows; i++)
+  {
+    _matrix[i] = new float[_numColumns];
+    for (unsigned int j = 0; j < _numColumns; j++)
+    {
+      _matrix[i][j] = a[i][j];
+    }
+  }
 }
 
 matrix::~matrix()
 {
+  // free the allocated memory
+  for (int i = 0; i < _numRows; i++)
+  {
+    delete[] _matrix[i];
+  }
+  delete[] _matrix;
 }
-
+void matrix::show()
+{
+  for (unsigned int i = 0; i < _numRows; i++)
+  {
+    for (unsigned int j = 0; j < _numColumns; j++)
+    {
+      std::cout << _matrix[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
 #endif // MATRIX_HH
