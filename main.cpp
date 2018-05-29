e32617326a112593cf62026cd5b323c87494a4f6
matheust3 <matheus.toniolli@hotmail.com>
2018-05-29 19:56:07 -0400

    operator*

--- main.cpp
+++ main.cpp
@@ -4,8 +4,27 @@
 int main(int argc, char const *argv[])
 {
 
-    float a[2][3] = {{1, 2, 3}, {2, 3, 5}};
+    float a[2][4] = {{1, 2, 3, 2}, {4, 5, 6, 2}};
     matrix m = matrix(a);
+    std::cout << " m=\n";
     m.show();
+    float b[5][5];
+    matrix n = {{6, 5, 4}, {3, 2, 1}};
+    std::cout << " n=\n";
+    n.show();
+    n = m;
+    std::cout << " n=\n";
+    n.show();
+    matrix o = {{1, 2}, {2, 1}, {1, 1}};
+    m = o;
+    std::cout << " m=\n";
+    m.show();
+    std::cout << " n=\n";
+    n.show();
+    std::cout << " o=\n";
+    o.show();
+    matrix p = m * n;
+    std::cout << " p=\n";
+    p.show();
     return 0;
 }
